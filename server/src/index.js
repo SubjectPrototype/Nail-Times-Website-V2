4fde07c01c5436afda80af75c191286e9fd5a692
SubjectPrototype <ryanhoang03@gmail.com>
2026-02-26 19:21:10 -0600

    bcrypt to bcryptjs

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const crypto = require("crypto");
 const express = require("express");
 const cors = require("cors");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { z } = require("zod");
 
